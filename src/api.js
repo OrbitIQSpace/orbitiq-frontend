f0bf8e6dddd965c1f6fd13ef6813485859f61fa0
Tyler Brumbaugh <tyler@orbitiqspace.com>
2025-12-30 12:44:44 -0500

    API FIX

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://orbitiq-backend-v72n.onrender.com';
 
-if (import.meta.env.DEV) {
+if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:3000';
 }
 export default axios;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-
+const publishableKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+//import.meta.env.VITE_CLERK_PUBLISHABLE_KEY ||
 if (!publishableKey) {
   throw new Error('Missing Clerk Publishable Key — check your .env file');
 }
